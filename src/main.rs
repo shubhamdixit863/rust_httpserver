e51405847b088e27119b1b3ab9c68684e9bf124a
shubham <shubhamdixit863@gmail.com>
2024-09-17 22:49:12 +0530

    http ok response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use std::io::{BufRead, BufReader, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::net::TcpStream;
 
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -13,6 +27,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_connection(_stream)
             }
             Err(e) => {
                 println!("error: {}", e);
