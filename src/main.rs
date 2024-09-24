0fb6d2a58efa005c4988c33cdbdceb3f5d1c1eb5
shubham <shubhamdixit863@gmail.com>
2024-09-25 00:10:24 +0530

    writing to file

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,134 @@
 use std::fmt::format;
-use std::io::{BufRead, BufReader, Write};
+use std::fs;
+use std::io::{BufRead, BufReader, Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::{env, thread};
-use std::fs;
+use std::fs::File;
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let mut buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader.by_ref()
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let user_agent = http_request.iter()
+
+
+    let user_agent = http_request
+        .iter()
         .find(|&header| header.starts_with("User-Agent: "))
         .map(|header| header.trim_start_matches("User-Agent: "))
         .unwrap_or("User-Agent not found");
     let collection: Vec<&str> = http_request[0].split(" ").collect();
-    // Split this route too
-    let route_part:Vec<&str>=collection[1].split("/").collect();
-    if route_part[0]=="" &&  route_part[1]==""{
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let route_part: Vec<&str> = collection[1].split("/").collect();
+    if collection[0] == "GET" {
+        // Split this route too
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }else if route_part[0]=="" && route_part[1]=="echo"   {
-        // send the body
-        let response = format!(
-            "HTTP/1.1 200 OK\r\n\
+        if route_part[0] == "" && route_part[1] == "" {
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+            stream.write_all(response.as_bytes()).unwrap();
+        } else if route_part[0] == "" && route_part[1] == "echo" {
+            // send the body
+            let response = format!(
+                "HTTP/1.1 200 OK\r\n\
         Content-Type: text/plain\r\n\
         Content-Length: {}\r\n\
         Connection: close\r\n\r\n\
         {}",
-            route_part[2].len(),
-            route_part[2]
-        );
-        stream.write_all(response.as_bytes()).unwrap();
-
-    } else if route_part[0]=="" && route_part[1]=="user-agent"{
-        let response = format!(
-            "HTTP/1.1 200 OK\r\n\
+                route_part[2].len(),
+                route_part[2]
+            );
+            stream.write_all(response.as_bytes()).unwrap();
+        } else if route_part[0] == "" && route_part[1] == "user-agent" {
+            let response = format!(
+                "HTTP/1.1 200 OK\r\n\
         Content-Type: text/plain\r\n\
         Content-Length: {}\r\n\
         Connection: close\r\n\r\n\
         {}",
-            user_agent.len(),
-            user_agent
-        );
-        stream.write_all(response.as_bytes()).unwrap();
+                user_agent.len(),
+                user_agent
+            );
+            stream.write_all(response.as_bytes()).unwrap();
+        } else if route_part[0] == "" && route_part[1] == "files" {
+            let env_args: Vec<String> = env::args().collect();
+            let mut dir = env_args[2].clone();
+            dir.push_str(&route_part[2]);
+            let file = fs::read(dir);
 
+            // Read the file whose name is route_part[1]
 
-    }else if route_part[0]=="" && route_part[1]=="files" {
-        let env_args: Vec<String> = env::args().collect();
-        let mut dir = env_args[2].clone();
-        dir.push_str(&route_part[2]);
-        let file = fs::read(dir);
-
-        // Read the file whose name is route_part[1]
-
-
-        match file {
-            Ok(_contents)=>{
-                let response = format!(
-                    "HTTP/1.1 200 OK\r\n\
+            match file {
+                Ok(_contents) => {
+                    let response = format!(
+                        "HTTP/1.1 200 OK\r\n\
          Content-Type: application/octet-stream\r\n\
         Content-Length:  {}\r\n\
         Connection: close\r\n\r\n\
         {}",
-                    _contents.len(),
-                    String::from_utf8(_contents).expect("file content")
-                );
-                stream.write_all(response.as_bytes()).unwrap();
-
-            },
-            Err(_e)=>{
-                println!("{:?}",_e);
-                let response = format!(
-                    "HTTP/1.1 404 Not Found\r\n\
+                        _contents.len(),
+                        String::from_utf8(_contents).expect("file content")
+                    );
+                    stream.write_all(response.as_bytes()).unwrap();
+                }
+                Err(_e) => {
+                    println!("{:?}", _e);
+                    let response = format!(
+                        "HTTP/1.1 404 Not Found\r\n\
         Content-Type: text/plain\r\n\
         Content-Length:  {}\r\n\
         Connection: close\r\n\r\n\
         {}",
-                    0,
-                    ""
-                );
-                stream.write_all(response.as_bytes()).unwrap();
-
-
+                        0, ""
+                    );
+                    stream.write_all(response.as_bytes()).unwrap();
+                }
             }
+        } else {
+            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    } else if collection[0] == "POST" {
+
+        // Find the Content-Length header
+        let content_length = http_request
+            .iter()
+            .find(|line| line.starts_with("Content-Length:"))
+            .and_then(|line| line.split(": ").nth(1))
+            .and_then(|len| len.parse::<usize>().ok())
+            .unwrap_or(0);
+
+        // Read the body (binary data)
+        let mut body = vec![0u8; content_length];
+        buf_reader.read_exact(&mut body).unwrap();
+        let content=String::from_utf8(body).unwrap();
+
+        if route_part[0] == "" && route_part[1] == "files"{
+             let file_name=route_part[2];
+            let mut file = File::create(file_name).unwrap();
+
+            // Write some data to the file
+            file.write_all(content.as_ref()).unwrap();
+
+            let response = format!(
+                "HTTP/1.1 200 OK\r\n\
+        Content-Type: text/plain\r\n\
+        Content-Length: {}\r\n\
+        Connection: close\r\n\r\n\
+        {}",
+                user_agent.len(),
+                user_agent
+            );
+            stream.write_all(response.as_bytes()).unwrap();
+
+
         }
 
-
-
-
-
-
     }
-
-
-    else {
-        let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-
-
 }
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -117,12 +141,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                thread::spawn( || {
-                    handle_connection(_stream)
-                });
-
-
-
+                thread::spawn(|| handle_connection(_stream));
             }
             Err(e) => {
                 println!("error: {}", e);
